b61f88273b00301337dc5ec6fabee16d2ba0a62d
EthanL02 <102267044+EthanL02@users.noreply.github.com>
2026-01-29 15:10:03 -0500

    Apply gravity to player

--- player.gd
+++ player.gd
@@ -1,16 +1,12 @@
 extends CharacterBody2D
 
+const GRAVITY = 200
+
 @export var jump_strength = 400
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
-	var velocity = Vector2.ZERO
-	if Input.is_action_just_pressed("Flap"):
-		velocity.y -= jump_strength
-		
-	position += velocity * delta
+func _physics_process(delta: float):
+	velocity.y += delta * GRAVITY
+	
+	var motion = velocity * delta
+	move_and_collide(motion)
